6a13784b7c611cfa9ff29c5dd3a5107c052d5063
Kelli Waiss <kelliwaiss@hotmail.com>
2016-01-08 12:14:55 -0600

    added post route and growls erb file

--- starter/app.rb
+++ starter/app.rb
@@ -3,31 +3,36 @@ require 'sinatra/reloader'
 
 enable :sessions
 
-
 before do
-session[:past_growls] ||= []
+    session[:past_growls] ||= []
+end
+
+get '/' do
+    erb :index
+end
+
+get '/growls' do
+    @growl= session[:past_growls]
+    erb :growls
+end
+
+post '/growls' do
+    growls = params[:growlers]
+
+    session[:past_growls].push growls #growls are stored in the past_growls array in the session hash
+
+    redirect '/' #go back to home page
+end
 
 # if value in textbox send to cart
 # viewing cart is optional on 1st page
 # value in text box cannot excede 142 characters
-end
-
 
 # Routes
-get '/' do
-
 # First -> Second page
 # create growl
 # view history of growls
 
 # return from second to first page
-  erb :index
-end
 
 
-get '/growls' do
-
-  erb :growls
-    @growl= session[:past_growls]
-
-end
