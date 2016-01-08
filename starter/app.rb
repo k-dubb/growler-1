3b986b4cb564faca9d97c852c79a326efe415a42
Joe Rizzo <hi.imjoerizzo@gmail.com>
2016-01-08 11:56:12 -0600

    growler

--- starter/app.rb
+++ starter/app.rb
@@ -1,12 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-before do
+enable :sessions
 
+
+before do
+session[:past_growls] ||= []
+
+# if value in textbox send to cart
+# viewing cart is optional on 1st page
+# value in text box cannot excede 142 characters
 end
 
-# Routes
-get '/' do 
 
-	erb :index
+# Routes
+get '/' do
+
+# First -> Second page
+# create growl
+# view history of growls
+
+# return from second to first page
+  erb :index
+end
+
+
+get '/growls' do
+
+  erb :growls
+    @growl= session[:past_growls]
+
 end
